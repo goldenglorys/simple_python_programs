635cee49a84157c36b7100ce6d56c3049cbe2377
Olusola Glory <olusolagloryolamide@gmail.com>
2020-08-20 17:00:30 +0100

    bitwise function added

--- bitwise.py
+++ bitwise.py
@@ -14,4 +14,20 @@ def binary_to_decimal():
     print(result)
 
 
-binary_to_decimal()
+# binary_to_decimal()
+
+
+def bitwise():
+#===== BINARY AND ========
+    a = 50      #110010
+    b = 25      #011001
+    c = a & b   #010000
+    print(c)
+
+#===== BINARY RIGHT SHIFT =====
+    x = 240     #11110000
+    y = x >> 2  #00111100
+    print(y)
+
+
+bitwise()
