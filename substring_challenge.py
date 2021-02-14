aeb57215d84f5ac63d72d14eae74917054159c0d
goldenglorys <olusolagloryolamide@gmail.com>
2021-02-14 18:47:36 +0100

    Substring window challenge for fourth brain completed

--- substring_challenge.py
+++ substring_challenge.py
@@ -1,42 +1,78 @@
-#
-# no_of_chars = 256
-#
-#
-# def findSubString(input):
-#     stringN = input[0]
-#     stringK = input[1]
-#
-#     hash_pat = [0] * no_of_chars
-#     hash_str = [0] * no_of_chars
-#
-#     for i in range(0, len(input[1])):
-#         hash_pat[ord(stringK[i])] += 1
-#
-#     start, start_index, min_len = 0, -1, float('inf')
-#     count = 0
-#     for j in range(0, len(input[0])):
-#         hash_str[ord(input[0][j])] += 1
-#         if hash_str[ord(input[0][j])] <= hash_pat[ord(input[0][j])]:
-#             count += 1
-#
-#         if count == stringK:
-#             while (hash_str[ord(input[0][start])] >
-#                    hash_pat[ord(input[0][start])] or
-#                    hash_pat[ord(input[0][start])] == 0):
-#                 if (hash_str[ord(input[0][start])] >
-#                         hash_pat[ord(input[0][start])]):
-#                     hash_str[ord(input[0][start])] -= 1
-#                 start += 1
-#             len_str = j - start + 1
-#             if min_len > len_str:
-#                 min_len = len_str
-#                 start_index = start
-#     if start_index == -1:
-#         print("No such strings")
-#         return ''
-#     return input[0][start_index: start_index + min_len]
-#
-#
-# if __name__ == "__main__":
-#     print(findSubString(["ahffaksfajeeubsne", "jefaa"]))
+# Python3 program to find the smallest window
+# containing all characters of a pattern.
+no_of_chars = 256
+
+
+# Function to find smallest window
+# containing all characters of 'pat'
+def findSubString(string, pat):
+    strLen1 = len(string)
+    strLen2 = len(pat)
+
+    # Check if string's length is
+    # less than pattern's
+    # length. If yes then no such
+    # window can exist
+    if strLen1 < strLen2:
+        print("No such window exists")
+        return ""
+
+    hash_pat = [0] * no_of_chars
+    hash_str = [0] * no_of_chars
+
+    # Store occurrence ofs characters of pattern
+    for i in range(0, strLen2):
+        hash_pat[ord(pat[i])] += 1
+
+    start, start_index, min_len = 0, -1, float('inf')
+
+    # Start traversing the string
+    count = 0  # count of characters
+    for j in range(0, strLen1):
+
+        # count occurrence of characters of string
+        hash_str[ord(string[j])] += 1
+
+        # If string's char matches with
+        # pattern's char then increment count
+        if (hash_str[ord(string[j])] <=
+                hash_pat[ord(string[j])]):
+            count += 1
+
+        # if all the characters are matched
+        if count == strLen2:
+
+            # Try to minimize the window
+            while (hash_str[ord(string[start])] >
+                   hash_pat[ord(string[start])] or
+                   hash_pat[ord(string[start])] == 0):
+
+                if (hash_str[ord(string[start])] >
+                        hash_pat[ord(string[start])]):
+                    hash_str[ord(string[start])] -= 1
+                start += 1
+
+            # update window size
+            len_window = j - start + 1
+            if min_len > len_window:
+                min_len = len_window
+                start_index = start
+
+    # If no window found
+    if start_index == -1:
+        print("No such window exists")
+        return ""
+
+    # Return substring starting from
+    # start_index and length min_len
+    return string[start_index: start_index + min_len]
+
+
+# Driver code
+if __name__ == "__main__":
+    string = "aaffhkksemckelloe"
+    pat = "sckemolel"
+
+    print("Smallest window is : ")
+    print(findSubString(string, pat))
 
